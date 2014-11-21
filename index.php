117e444db010fe3e53ac6f784d2b81b5e6bfb8f1
Michał Darkowski <mastrodarko@gmail.com>
2014-11-20 23:59:00 -0600

    Aga popsuła więc naprawiam index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$xml=simplexml_load_file("resources/baza_dowcipow.xml") or die("Blad: Nie mozna wczytac pliku z dowcipami.");
+$xml=simplexml_load_file("resources/katalog_dowcipow.xml") or die("Blad: Nie mozna wczytac pliku z dowcipami.");
 foreach($xml->children() as $dowcip) {
     echo $dowcip . "<br /><br />";
 }
